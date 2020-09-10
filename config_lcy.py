11c95c1b533f62fc5e2e75d1d26b79ac3d048bfd
lee.lcy <lee.lcy@alibaba-inc.com>
2020-09-10 18:57:51 +0800

    test

--- config_lcy.py
+++ config_lcy.py
@@ -7,8 +7,8 @@ WD = 1e-4
 SAVE_FREQ = 1
 resume = ''
 # test_model = './work_dirs/dami_tmp/20200904_093822/025.ckpt'
-# test_model = './work_dirs/dami_crop_0.5_tmp/20200908_235500/058.ckpt'
-test_model = './work_dirs/dami_crop_0.3_tmp/20200909_002318/056.ckpt'
+test_model = './work_dirs/dami_crop_0.5_tmp/20200908_235500/058.ckpt'
+# test_model = './work_dirs/dami_crop_0.3_tmp/20200909_002318/056.ckpt'
 
 
 
